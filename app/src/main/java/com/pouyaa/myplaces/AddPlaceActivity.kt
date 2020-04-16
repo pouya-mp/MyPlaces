b101b1623fbacd57619017a1027287d0fe86ff50
Pouya <pouyaa91@yahoo.com>
2020-04-16 17:19:51 +0430

    Add : DatePicker

--- app/src/main/java/com/pouyaa/myplaces/AddPlaceActivity.kt
+++ app/src/main/java/com/pouyaa/myplaces/AddPlaceActivity.kt
@@ -1,12 +1,32 @@
 package com.pouyaa.myplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_place.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPlaceActivity : AppCompatActivity() {
+
+//    private var cal = Calendar.getInstance()
+//    private lateinit var dateSetListener : DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_place)
+        setSupportActionBar(addPlaceToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        addPlaceToolbar.setNavigationOnClickListener { onBackPressed() }
+
+
+        addDateEditText.setOnClickListener {
+
+            PickDate(addDateEditText).selectDate(this)
+
+        }
+
+
     }
+
 }
--- app/src/main/java/com/pouyaa/myplaces/MainActivity.kt
+++ app/src/main/java/com/pouyaa/myplaces/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.pouyaa.myplaces
 
+import android.app.DatePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_add_place.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fabAddPlace.setOnClickListener {
+            val intent = Intent(this,AddPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
